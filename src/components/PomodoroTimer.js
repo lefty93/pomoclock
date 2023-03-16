5e1b01e4d031421ae2e880cd5e14dc547d5165a6
Chu Seng <yongchuseng@gmail.com>
2023-03-16 16:43:01 +0800

    First commit.

---
+++ src/components/PomodoroTimer.js
@@ -0,0 +1,55 @@
+import React, {useState, useEffect} from 'react'
+
+function PomodoroTimer() {
+    const [time, setTime] = useState(25*60)
+    const [isRunning, setIsRunning] = useState(false)
+    let startpauseaudio = new Audio('/audio/switch-1.wav')
+    let resetaudio = new Audio('/audio/button-17.wav')
+
+    useEffect(() => {
+        let timerId;
+        if (isRunning && time > 0) {
+            timerId = setInterval(() => {
+                setTime(prevTime => prevTime - 1);
+            }, 1000)
+        } else if (time === 0) {
+            setIsRunning(false);
+        }
+        return () => clearInterval(timerId);
+    }, [isRunning, time]);
+
+    useEffect(() => {
+        document.title = `${formatTime(time)} - Focus Time`
+    })
+
+    function handleStart() {
+        setIsRunning(prevIsRunning => !prevIsRunning)
+        startpauseaudio.play()
+    }
+
+    function handleReset() {
+        setIsRunning(false);
+        setTime(25 * 60)
+        resetaudio.play()
+    }
+
+    function formatTime(timeInSeconds) {
+      const minutes = Math.floor(timeInSeconds / 60);
+      const seconds = timeInSeconds % 60;
+      return `${minutes.toString().padStart(2, "0")}:${seconds
+        .toString()
+        .padStart(2, "0")}`;
+    }
+
+  return (
+      <>
+          <h1>Pomodoro Timer</h1>
+      <h2>{formatTime(time)}</h2>
+      <button onClick={handleStart}>{isRunning ? 'Pause' : 'Start'}</button>
+      <button onClick={handleReset}>Reset</button>
+    
+    </>
+  )
+}
+
+export default PomodoroTimer
